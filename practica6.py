f8ae634c296b0bff86d5b5d764b878857d8e0f8f
romi-02 <marin.danha.2024@cbtis-246.edu.mx>
2025-02-19 17:33:07 -0600

    Add files via upload

---
+++ practica6.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+tupla_1 =(1,2,3,4,5)
+print(tupla_1)
+
+
+# In[3]:
+
+
+tupla_2 = (6,7,8,9,10)
+print(tupla_2[2])
+
+
+# In[5]:
+
+
+tupla_1 = (1,2,3,4,5)
+tupla_2 = (6,7,8,9,10)
+tupla_nueva= tupla_1 + tupla_2
+print(tupla_nueva)
+
+
+# In[ ]:
+
+
+tupla
+
