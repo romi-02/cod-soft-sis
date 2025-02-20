fe45712ae45093e9259c823654b797448e2d7cff
romi-02 <marin.danha.2024@cbtis-246.edu.mx>
2025-02-20 15:27:50 -0600

    Add files via upload

---
+++ practica2.py
@@ -0,0 +1,26 @@
+#Materia: CodSofSisIn
+#Fecha: 20/02/25
+#Alumno: Danha Romina Marín Ventura
+#Grupo: 2J
+#Nombre de la practica: Listas y tuplas
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+nombre = input("cual es tu nombre")
+print("hola ", nombre)
+apellido = input("cual es tu apellido")
+print("hola ",nombre,' ', apellido)
+print("tu nombre invertido es ",nombre[::-1])
+print("tu apellido invertido es ", apellido[::-1])
+
+
+# In[ ]:
+
+
+
+
