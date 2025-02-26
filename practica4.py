b4a373cc53d08f342cfb938a2a7645feee0827a5
romi-02 <marin.danha.2024@cbtis-246.edu.mx>
2025-02-26 16:53:43 -0600

    Add files via upload

---
+++ practica4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+uno=input("ingresa tu nombre")
+dos=input("ingresa tu edad")
+tres=input("ingresa que quieres ser")
+print("mi nombre es",uno,"tengo",dos,"años","y quiero ser",tres)
+
