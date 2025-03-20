b3b4ddba4eb4e128c6ead1f13ea484f2f564acdf
romi-02 <marin.danha.2024@cbtis-246.edu.mx>
2025-03-20 14:58:56 -0600

    Add files via upload

---
+++ Segundo parcial/ejercicio3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Musica:
+   pass
+class Instrumentos(Musica):
+    pass
+class Canciones:
+    pass
+class Cantantes(Instrumentos,Canciones):
+    pass
+    
+
+
+# In[ ]:
+
+
+
+
