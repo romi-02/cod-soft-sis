98d0082569968d7cefc02520c74991f6bc714260
romi-02 <marin.danha.2024@cbtis-246.edu.mx>
2025-05-11 21:54:00 -0600

    Add files via upload

---
+++ Segundo parcial/Practica3_segundo_parcial.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+#Abuelo
+class auto:
+    pass
+#Padre
+class motores(auto):
+    pass
+#Madre
+class turbocomprensores:
+    pass
+#Hijo
+class vehículos(motores, turbocomprensores):
+    pass
+
