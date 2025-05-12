05d6263e187ed0b33a5dcefbf9a412de692690dc
romi-02 <marin.danha.2024@cbtis-246.edu.mx>
2025-05-11 21:50:40 -0600

    Add files via upload

---
+++ Segundo parcial/Practica1-2°doPeriodo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+#Tipo: Artista con 2 atributos de instancia y de clase.
+class Artista:
+    concierto = 'Estadio XX'
+    horario = '7:00 a 9:30'
+    
+    def _init_(self, nombre, grupo):
+        print(f"Personalizando artista {nombre}, { grupo_musical}")
+    
+        self.nombre = nombre
+        self.grupo_musical = grupo_musical
+un_artista = Artista("JVKE", "Golden Hour")
+
+
+# In[ ]:
+
+
+
+
