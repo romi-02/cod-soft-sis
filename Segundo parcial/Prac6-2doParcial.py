20c1d6684fc033b8b641c034f4d07cb2f442bd0c
romi-02 <marin.danha.2024@cbtis-246.edu.mx>
2025-05-11 21:55:56 -0600

    Add files via upload

---
+++ Segundo parcial/Prac6-2doParcial.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[27]:
+
+
+#Script
+class MiPractica():
+    def  mi_primer_metodo(self, variable_a, variable_b):
+        
+        return (variable_a + variable_b) / CONSTANTE_USADA
+
+el_resultado = MiPractica()
+print(el_resultado.mi_primer_metodo(5, 5))
+
